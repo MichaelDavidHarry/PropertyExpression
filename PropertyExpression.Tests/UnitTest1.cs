f14cbb6640a5fafdc825814a0189edb4a9e0c47e
michaeldavidharry <michael@compilefailed.com>
2020-03-21 18:06:19 -0500

    Add unit test project.

---
+++ PropertyExpression.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PropertyExpression.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
